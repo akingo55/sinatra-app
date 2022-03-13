74c2d6fe668d9e8fc8a6648a8f38c6b5ff9d58dc
akingo55 <am1157154@gmail.com>
2022-03-13 15:49:19 +0900

    update variable name from 'data' to 'memo_data'

--- memo_app.rb
+++ memo_app.rb
@@ -6,16 +6,16 @@ require 'erb'
 require 'json'
 
 file = 'output.json'
-data = JSON.parse(File.read(file))
+memo_data = JSON.parse(File.read(file))
 
 get '/' do
-  @data = data
+  @data = memo_data
   erb :top
 end
 
 get '/memos/:id' do
   @id = params['id'].to_i
-  @data = data
+  @data = memo_data
   erb :show
 end
 
@@ -26,7 +26,7 @@ end
 post '/memos' do
   File.open(file, 'w') do |f|
     count = data['memos'].last['id'] + 1
-    data['memos'] << { 'id' => count, 'title' => params[:title], 'content' => params[:content] }
+    memo_data['memos'] << { 'id' => count, 'title' => params[:title], 'content' => params[:content] }
     f.puts(data.to_json)
   end
   redirect '/'
@@ -34,29 +34,29 @@ end
 
 get '/memos/:id/edit' do
   @id = params['id'].to_i
-  @data = data
+  @data = memo_data
   erb :edit
 end
 
 patch '/memos/:id' do
   @id = params['id'].to_i
-  memo = data['memos'].find { |memo| memo['id'] == @id }
+  memo = memo_data['memos'].find { |memo| memo['id'] == @id }
 
   memo['title'] = params[:title]
   memo['content'] = params['content']
 
   File.open(file, 'w') do |f|
-    f.puts(data.to_json)
+    f.puts(memo_data.to_json)
   end
   redirect '/'
 end
 
 delete '/memos/:id' do
   @id = params['id'].to_i
-  data['memos'].delete_if { |memo| memo['id'] == @id }
+  memo_data['memos'].delete_if { |memo| memo['id'] == @id }
 
   File.open(file, 'w') do |f|
-    f.puts(data.to_json)
+    f.puts(memo_data.to_json)
   end
 
   redirect '/'
