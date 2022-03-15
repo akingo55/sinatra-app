329925709aa368918f349e9d7c43c9d4073ca7c5
akingo55 <am1157154@gmail.com>
2022-03-16 00:07:36 +0900

    change 'memo_list' to 'memos'

--- memo_app.rb
+++ memo_app.rb
@@ -22,13 +22,13 @@ helpers do
 end
 
 get '/' do
-  @memo_list = memo_data
+  @memos = memo_data
   erb :top
 end
 
 get '/memos/:id' do
   @id = params['id'].to_i
-  @memo_list = memo_data
+  @memos = memo_data
   erb :show
 end
 
@@ -37,42 +37,42 @@ get '/new' do
 end
 
 post '/memos' do
-  memo_list = memo_data
+  memos = memo_data
   File.open(FILE, 'w') do |f|
-    count = memo_list['memos'].empty? ? 1 : memo_list['memos'].last['id'].to_i + 1
-    memo_list['memos'] << { id: count, title: h(params[:title]), content: h(params[:content]) }
-    f.puts(memo_list.to_json)
+    count = memos['memos'].empty? ? 1 : memos['memos'].last['id'].to_i + 1
+    memos['memos'] << { id: count, title: h(params[:title]), content: h(params[:content]) }
+    f.puts(memos.to_json)
   end
   redirect '/'
 end
 
 get '/memos/:id/edit' do
   @id = params['id'].to_i
-  @memo_list = memo_data
+  @memos = memo_data
   erb :edit
 end
 
 patch '/memos/:id' do
   @id = params['id'].to_i
-  memo_list = memo_data
-  memo = memo_list['memos'].find { |hash| hash['id'] == @id }
+  memos = memo_data
+  memo = memos['memos'].find { |hash| hash['id'] == @id }
 
   memo['title'] = h(params[:title])
   memo['content'] = h(params['content'])
 
   File.open(FILE, 'w') do |f|
-    f.puts(memo_list.to_json)
+    f.puts(memos.to_json)
   end
   redirect '/'
 end
 
 delete '/memos/:id' do
   @id = params['id'].to_i
-  memo_list = memo_data
-  memo_list['memos'].delete_if { |memo| memo['id'] == @id }
+  memos = memo_data
+  memos['memos'].delete_if { |memo| memo['id'] == @id }
 
   File.open(FILE, 'w') do |f|
-    f.puts(memo_list.to_json)
+    f.puts(memos.to_json)
   end
 
   redirect '/'
